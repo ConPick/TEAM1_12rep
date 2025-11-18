1725b811c92c9f23ee2f74bc3ed9c06b667fbed5
syc <ryon0316@naver.com>
2025-11-18 11:52:25 +0900

    duplicated code TASK3

--- streamlit.py
+++ streamlit.py
@@ -48,13 +48,15 @@ else:
 msg = st.chat_input("메시지를 입력하세요")
 
 # ================================
-# Task 3: 차트그리기
+# Task 3: 차트그리기 
 # ================================
 from numpy.random import default_rng as rng
 df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
 st.area_chart(df, x_label='areaX', y_label='areaY')
 st.line_chart(df, x_label='lineX', y_label='lineY')
 st.bar_chart(df, x_label='barX', y_label='barY')
+
+
 # ================================
 # Task 4: CSV 업로드
 # ================================
